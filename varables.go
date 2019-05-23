cd22abdf7396bc0c646f7858fcb050bcda32f67a
bensigo <sigocode@gmail.com>
2019-05-24 00:39:33 +0400

    variable in go lang

---
+++ varables.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	// creating a variable
+	var name = "Sigo"
+	fmt.Println(name)
+	// constant
+	const note = "I'm a computer programer!"
+	// another way of creating a variable
+	age := 25
+	fmt.Println(age)
+	// creating variable with data type
+	var job string
+	job = "software Engineer"
+	fmt.Println(job)
+}
